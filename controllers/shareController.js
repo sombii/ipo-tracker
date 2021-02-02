7cfeacf8254f45acf8ed72cffd05a05ce18e62c2
sombii <blackknightstd@gmail.com>
2021-02-02 17:08:32 +0545

    - implemented share controller (almost done)

--- controllers/shareController.js
+++ controllers/shareController.js
@@ -1,5 +1,5 @@
-
 var Share = require("../models/share");
+const {body, validationResult} = require("express-validator");
 
 // list all securities on GET request
 exports.list_all_securities = function (req, res, next) {
@@ -41,6 +41,74 @@ exports.list_individual_share_type = (req, res, next) => {
 //post controllers
 
 // create new any type of issue
-exports.create_new_issue = (req, res, next) => {
-    res.send("not implemented yet");
-};
+exports.create_new_issue = [
+
+    //validating and sanitizing data
+    body("company", "Company must not be empty.").trim().isLength({min: 1}).escape(),
+    body("issue_name", "Issue name must not be empty.").trim().isLength({min: 1}).escape(),
+    body("issue_opening_date", "Invalid date").optional({checkFalsy: true}).toDate(),
+    body("issue_closing_date", "Invalid date").optional({checkFalsy: true}).toDate(),
+    body("issue_last_closing_date", "Invalid date").optional({checkFalsy: true}).toDate(),
+    body("issue_manager", "Issue manager must not be empty.").trim().isLength({min: 1}).escape(),
+    body("issue_type", "Issue type must not be empty.").trim().isLength({min: 1}).escape(),
+    body("issued_unit", "Issued unit must not be empty.").trim().isLength({min: 1}).escape(),
+    body("min_unit", "Minimum unit must not be empty.").trim().isLength({min: 1}).escape(),
+    body("listing_date", "Invalid date").optional({checkFalsy: true}).toDate(),
+    body("per_unit", "Per unit must not be empty.").trim().isLength({min: 1}).escape(),
+    body("ratio", "Ratio must not be empty.").trim().isLength({min: 0}).escape(),
+    body("scrip", "Scrip must not be empty.").trim().isLength({min: 1}).escape(),
+    body("sector", "Sector must not be empty.").trim().isLength({min: 1}).escape(),
+    body("share_type", "Share type must not be empty.").trim().isLength({min: 1}).escape(),
+    body("status", "Status must not be empty.").trim().isLength({min: 1}).escape(),
+
+    (req, res, next) => {
+
+        const errors = validationResult(req);
+
+        const share = new Share({
+            company: req.body.company,
+            issue_name: req.body.issue_name,
+            issue_opening_date: req.body.issue_opening_date,
+            issue_closing_date: req.body.issue_closing_date,
+            issue_last_closing_date: req.body.issue_last_closing_date,
+            issue_manager: req.body.issue_manager,
+            issue_type: req.body.issue_type,
+            issued_unit: req.body.issued_unit,
+            min_unit: req.body.min_unit,
+            listing_date: req.body.listing_date,
+            per_unit: req.body.per_unit,
+            ratio: req.body.ratio,
+            scrip: req.body.scrip,
+            sector: req.body.sector,
+            share_type: req.body.share_type,
+            status: req.body.status,
+        });
+
+        if (!errors.isEmpty()) {
+            res.json(errors)
+        } else {
+            share.save(function (err, share) {
+                if (err) {
+                    return next(err)
+                }
+                res.json(share);
+            })
+        }
+    }
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
