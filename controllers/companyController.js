c1bbb008f01c291eddd6ed8c705e4fa298546cdf
sombii <blackknightstd@gmail.com>
2021-02-02 15:37:36 +0545

    logic to create company done

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -1,4 +1,5 @@
-var Company = require("../models/company");
+const Company = require("../models/company");
+const {body, validationResult} = require("express-validator");
 
 //list all companies
 exports.list_all = (req, res, next) => {
@@ -7,11 +8,34 @@ exports.list_all = (req, res, next) => {
             if (err) {
                 return next(err);
             }
-            res.json(company_list);
+            const com = company_list.map(c => {
+                return {name: c.name, established: c.established, established_formatted: c.established_formatted}
+            })
+            res.send(com);
         });
 }
 
 // create new company
-exports.create_new_company = (req, res, next) => {
+exports.create_new_company = [
 
-};
+    body("name", "Name must be specified").isString().trim().isLength({min: 1}).escape(),
+    body("established", "Invalid date").optional({checkFalsy: true}).toDate(),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+        const company = new Company({
+            name: req.body.name,
+            established: req.body.established
+        });
+        if (!errors.isEmpty()) {
+            res.json(errors);
+        } else {
+            company.save(function (err, company) {
+                if (err) {
+                    return next(err)
+                }
+                res.json(company)
+            })
+        }
+    }
+]
--- models/company.js
+++ models/company.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
+const {DateTime} = require("luxon");
 
 const CompanySchema = mongoose.Schema({
     name: {type: String, required: true},
-    established: {type: Date}
+    established: {type: Date, default: Date.now}
 });
+
+CompanySchema.virtual("established_formatted")
+    .get(function () {
+        return DateTime.fromJSDate((this.established)).toLocaleString(DateTime.DATE_MED);
+    })
 
 module.exports = mongoose.model("Company", CompanySchema, "company");
